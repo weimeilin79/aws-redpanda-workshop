ff78fd68ede3a3a3bdbf7dd726dd94ed098a5369
weimeilin79 <weimeilin@gmail.com>
2024-06-13 20:26:30 -0400

    upload frontend code for workshop

---
+++ frontend/index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const { Kafka } = require('kafkajs');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const brokers = process.env.REDPANDA_BROKERS ? process.env.REDPANDA_BROKERS.split(',') : ['localhost:19092'];
+const saslMechanism = process.env.REDPANDA_SASL_MECHANISM || 'SCRAM-SHA-256';
+const username = process.env.REDPANDA_USERNAME || 'workshop';
+const password = process.env.REDPANDA_PASSWORD || '1234qwer';
+
+const kafka = new Kafka({
+    clientId: 'rpg-frontend',
+    brokers: brokers,
+    ssl: {},
+    sasl: {
+        mechanism: saslMechanism,
+        username: username,
+        password: password
+    }
+});
+
+const producer = kafka.producer();
+const consumer = kafka.consumer({ groupId: 'rpg-group' });
+  
+const setupKafka = async () => {
+    await producer.connect();
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'rpg-response' });
+
+    consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+          const value = message.value.toString();  // Convert buffer to string
+          const jsonData = JSON.parse(value);  // Parse JSON string to an object
+          io.emit('receive-message', jsonData);  // Emit as a JavaScript object
+        },
+      });
+  };
+
+  setupKafka();
+
+  io.on('connection', (socket) => {
+    socket.on('send-message', async (data) => {
+      //io.emit('receive-message', data); 
+      await producer.send({
+        topic: 'npc-request',
+        messages: [
+          { value: JSON.stringify(data) }
+        ],
+      });
+    });
+});
+
+app.use(express.static('public'));
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
